7722fd7fc543e57a9608d420bf40ef644044756b
Damilola Olubunmi <damilolaolubunmi60@gmail.com>
2026-01-16 11:04:07 +0100

    Completed booleans.js

--- booleans/booleans.js
+++ booleans/booleans.js
@@ -6,7 +6,7 @@
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let newBoolean
+let newBoolean = true
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -25,9 +25,9 @@ let dadsHeight = generateRandomNumber(100, 200)
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let momIsTaller
-let dadIsTaller
-let areTheSameHeight
+let momIsTaller = momsHeight > dadsHeight
+let dadIsTaller = dadsHeight > momsHeight
+let areTheSameHeight = momsHeight === dadsHeight
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -46,7 +46,7 @@ let studentEssay = generateSudentEssay()
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let notPlagiarised
+let notPlagiarised = studentEssay.includes(keyPhrase) === false
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -68,10 +68,10 @@ let thresholdForTall = 150
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let siblingsAreTall
-let siblingsAreShort
-let atLeastOneIsTall
-let onlyOneIsTall
+let siblingsAreTall = sistersHeight >= thresholdForTall && brothersHeight >= thresholdForTall
+let siblingsAreShort = sistersHeight < thresholdForTall && brothersHeight < thresholdForTall
+let atLeastOneIsTall = sistersHeight >= thresholdForTall || brothersHeight >= thresholdForTall
+let onlyOneIsTall = (sistersHeight > thresholdForTall && brothersHeight < thresholdForTall) || (sistersHeight < thresholdForTall && brothersHeight > thresholdForTall)
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -94,7 +94,7 @@ let secretCode = generateSecretCode()
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let codeIsValid
+let codeIsValid = secretCode.charAt(4) >= 'A' && secretCode.charAt(4) <= 'Z' && secretCode.charAt(9) >= '0' && secretCode.charAt(9) <= '9' && secretCode.length === 15
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -115,13 +115,13 @@ let truthy1, truthy2
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-// falsy1 = Boolean("")
-// falsy2 = Boolean(0)
-// falsy3 = Boolean(undefined)
-// falsy4 = Boolean(null)
+falsy1 = Boolean("")
+falsy2 = Boolean(0)
+falsy3 = Boolean(undefined)
+falsy4 = Boolean(null)
 
-// truthy1 = Boolean("non-empty string")
-// truthy2 = Boolean(1)
+truthy1 = Boolean("non-empty string")
+truthy2 = Boolean(1)
 
 ////////////////////////////////////////////////////////////////////////
 
